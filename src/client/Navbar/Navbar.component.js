09e55b300ed7b7b5ca612b7dbd082c73ca19e907
Chan Park <chanpark93@gmail.com>
2020-03-25 23:15:49 -0700

    update navbar - dropdown isbroken

--- src/client/Navbar/Navbar.component.js
+++ src/client/Navbar/Navbar.component.js
@@ -1,19 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+
 
 import "./navbar.styles.css"
 
-const NavbarDesktop = () => {
+const Navbar = () => {
+  
+  const [toggle, setToggle] = React.useState(false)
+  
+  const handleClick = e => {
+    e.preventDefault()
+    setToggle(!toggle)
+    console.log(toggle)
+  }
+  
   return (
-    <header className={"navbar"}>
+    <header className="navbar">
       <div className="navbar-container">
         <Link className="home-icon" to='/'><div>P \ T</div></Link>
         <nav className="navbar-nav">
-          <ul className="navbar-ul">
-              <li><Link to='/about'>About</Link></li>
-              <li><Link to='/projects'>Projects</Link></li>
-              <li><Link to='/login'>Login</Link></li>
-              <li><a></a></li>
+              <div>
+                <li className="navbar-icon">
+                  <NavLink
+                    to="#"
+                    onClick={e=>handleClick(e)}
+                  >ICON</NavLink>
+                </li>
+              </div>
+              <ul className="navbar-ul">
+              <div className={toggle ? "navbar-others" : "navbar-others dropdown-navbar"}>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/projects'>Projects</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                
+              </div>
+              
           </ul>
         </nav>
         
@@ -23,4 +44,4 @@ const NavbarDesktop = () => {
 
   
 }
-export default NavbarDesktop
+export default Navbar
