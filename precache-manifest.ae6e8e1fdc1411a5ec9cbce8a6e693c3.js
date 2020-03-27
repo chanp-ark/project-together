337912001fd4aa641c8c10d445c216dfea7af5d9
Chan Park <chanpark93@gmail.com>
2020-03-27 16:40:15 -0700

    Updates

---
+++ precache-manifest.ae6e8e1fdc1411a5ec9cbce8a6e693c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96ffabbda9819374e2566894541d80f",
+    "url": "/project-together-frontend/index.html"
+  },
+  {
+    "revision": "b0056375eed6cee28177",
+    "url": "/project-together-frontend/static/css/main.c54d52d5.chunk.css"
+  },
+  {
+    "revision": "fa8071b3c88210f04103",
+    "url": "/project-together-frontend/static/js/2.2a8a1c9e.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/project-together-frontend/static/js/2.2a8a1c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0056375eed6cee28177",
+    "url": "/project-together-frontend/static/js/main.13f5c722.chunk.js"
+  },
+  {
+    "revision": "6f5549425dfd21d1ce58",
+    "url": "/project-together-frontend/static/js/runtime-main.e878e4db.js"
+  }
+]);
